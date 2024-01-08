b6fd87f487d647734faf5f3f4a405dbef5930201
Andrew Bakhmatov <40628201+AndBoyS@users.noreply.github.com>
2024-01-08 16:01:02 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Steps to do:
+
+- Run all scripts in scripts_to_execute
+- Activate windows https://kmsavto.cc/%d0%b0%d0%ba%d1%82%d0%b8%d0%b2%d0%b0%d1%82%d0%be%d1%80-windows-10/?ysclid=lr48trikvu318802468
+- Logins: yandex, github, google, spotify, steam
+- Sync VSCode
+- https://github.com/Sycnex/Windows10Debloater
---
+++ scripts_to_execute/programs.ps1
@@ -0,0 +1,8 @@
+winget install --id Git.Git -e --source winget
+winget install -e --id Fork.Fork
+winget install -e --id Microsoft.VisualStudioCode
+winget install --id=Yandex.Browser  -e
+winget install -e --id Valve.Steam
+winget install -e --id Telegram.TelegramDesktop
+winget install Spotify.Spotify
+SetDefaultBrowser\SetDefaultBrowser.exe HKCU Yandex.QA4SERKHGLGG33IW3RFILPCEGY
