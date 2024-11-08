5ce41e668d31fd3b8dc447073268dcaf56c1253a
AndBoyS <andboyxd@gmail.com>
2024-11-08 17:48:10 +0300

    bump

--- scripts_to_execute/programs.ps1
+++ scripts_to_execute/programs.ps1
@@ -1,2 +1,2 @@
-choco install git vscode steam telegram spotify Yandex-browser puntoswitcher -y
+choco install git vscode steam telegram spotify Yandex-browser puntoswitcher discord -y
 SetDefaultBrowser\SetDefaultBrowser.exe HKCU Yandex.QA4SERKHGLGG33IW3RFILPCEGY
